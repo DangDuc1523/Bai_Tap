ca8e8625ed7fdaae2e79abf4db261b8ce4b037a1
DangDuc1523 <133780382+DangDuc1523@users.noreply.github.com>
2023-05-23 14:20:48 +0700

    Update BaiTap2.java

--- BaiTap2.java
+++ BaiTap2.java
@@ -8,10 +8,17 @@ public class BT2_Slot5 {
         System.out.print("Nhap so luong phan tu cua mang: ");
         int n = sc.nextInt();
     int[] arr = new int[n];
+        // nhap so 
         System.out.println("Nhap so : ");
         for(int i = 0; i < n; i++){
          arr[i] = sc.nextInt();
         }
+        // in so trong mang
+        System.out.println("Mang bao gom : ");
+        for(int i = 0; i < n; i++){
+         System.out.print(arr[i] + " ");
+        }
+        // sap xep
         int test;
     for(int i = 0; i<n; i++){
       for(int j = i + 1 ; j<n;j++){
@@ -21,11 +28,12 @@ public class BT2_Slot5 {
          arr[j] = test;
       }
     }    
-    }
+  }
+    // in ra sau sap xep
     System.out.print("ket qua: ");
      for(int i=0; i<n; i++){
          System.out.print(arr[i]);
      }   
-    }
-    
-}
+    } 
+} 
+
