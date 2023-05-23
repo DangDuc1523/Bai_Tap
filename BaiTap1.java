f97adf42b1cb565ff1e356c99f08e1c7f64220f1
DangDuc1523 <133780382+DangDuc1523@users.noreply.github.com>
2023-05-23 14:04:43 +0700

    Create BaiTap1.java

---
+++ BaiTap1.java
@@ -0,0 +1,20 @@
+package test.at.pkgclass;
+
+public class BT1_Slot5 {
+    public static void main(String []args){
+        int a ;
+        int b = 2;
+      for( a = 2; a<= 10; a++){
+          if(a % 2 == 0){
+         System.out.print(a +" ");
+      }
+      }
+      System.out.println(" ");
+      while(b <= 10){
+      if(b % 2 == 0){
+         System.out.print(b +" ");
+      }
+      b++;
+      }
+    }
+}
